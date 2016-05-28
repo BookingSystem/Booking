f7e2ba5e64df73f5ce666fe188f4e612493f5e79
Arslan <arslan.abdulkadyrov@gmail.com>
2016-05-28 21:08:14 +0300

    hey

--- BookingTickets/BookingTickets/Form1.cs
+++ BookingTickets/BookingTickets/Form1.cs
@@ -17,5 +17,9 @@ namespace BookingTickets {
         private void Form1_Load(object sender, EventArgs e) {
 
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
     }
 }
